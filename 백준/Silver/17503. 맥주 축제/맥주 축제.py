acfc2e8e5c738a8c6d6b8a15c11f3db04cfe5a3a
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-02-29 00:41:37 +0900

    [Silver I] Title: 맥주 축제, Time: 3172 ms, Memory: 82716 KB -BaekjoonHub

--- 백준/Silver/17503. 맥주 축제/맥주 축제.py
+++ 백준/Silver/17503. 맥주 축제/맥주 축제.py
@@ -1,14 +1,16 @@
 import sys
+from heapq import heappush, heappop
 input = sys.stdin.readline
 
 
 def possible(now):
-    prefers = sorted(prefer for prefer, level in beers if level <= now)
-    return sum(prefers[-n:]) if len(prefers) >= n else -1
+    prefers = [prefer for prefer, level in beers if level <= now]
+    return sum(prefers[:n]) if len(prefers) >= n else -1
 
 
 n, m, k = map(int, input().split())
 beers = [list(map(int, input().split())) for _ in range(k)]  # 선호도 도수레벨
+beers.sort(key=lambda beer: -beer[0])
 
 INF = (k << 31) + 1
 left, right = 0, INF
