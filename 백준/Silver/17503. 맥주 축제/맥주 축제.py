85c00846fd318788c94cff556cb427b7df36a41a
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-02-29 00:25:14 +0900

    [Silver I] Title: 맥주 축제, Time: 3268 ms, Memory: 73744 KB -BaekjoonHub

---
+++ 백준/Silver/17503. 맥주 축제/맥주 축제.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+
+def possible(now):
+    prefers = sorted(prefer for prefer, level in beers if level <= now)
+    return sum(prefers[-n:]) if len(prefers) >= n else -1
+
+
+n, m, k = map(int, input().split())
+beers = [list(map(int, input().split())) for _ in range(k)]  # 선호도 도수레벨
+
+INF = (k << 31) + 1
+left, right = 0, INF
+while left < right:
+    mid = left + right >> 1
+    if possible(mid) >= m:
+        right = mid
+    else:
+        left = mid + 1
+
+print(left if left != INF else -1)
