0aada03b80cca71a171750ab066f5761283d94a8
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-03-01 16:26:19 +0900

    [Gold II] Title: 캔디캔디, Time: 524 ms, Memory: 35364 KB -BaekjoonHub

---
+++ 백준/Gold/2878. 캔디캔디/캔디캔디.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/2878
+import sys
+input = sys.stdin.readline
+MOD = (1 << 64) - 1
+
+m, n = map(int, input().split())
+candies = sorted(int(input()) for _ in range(n))
+
+left, right = 0, max(candies)
+while left < right:
+    mid = left + right >> 1
+    c = sum(map(lambda candy: max(0, candy-mid), candies))
+
+    if c <= m:
+        right = mid
+    else:
+        left = mid + 1
+
+rest = m-sum(map(lambda candy: max(0, candy-left), candies))
+ans = -(left**2 - (left-1)**2) * rest
+for candy in candies:
+    ans += min(candy, left) ** 2
+    ans &= MOD
+
+print(ans)
