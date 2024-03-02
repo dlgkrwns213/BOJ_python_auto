c80d8897052c4ca6993a7f96197ea083492343f9
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-03-02 12:57:34 +0900

    [Silver I] Title: 인형들, Time: 148 ms, Memory: 110648 KB -BaekjoonHub

--- 백준/Silver/15954. 인형들/인형들.py
+++ 백준/Silver/15954. 인형들/인형들.py
@@ -4,12 +4,21 @@ n, k = map(int, input().split())
 prefers = list(map(int, input().split()))
 
 mn = float('inf')
-for i in range(n):
-    for j in range(i+k-1, n):
-        cnt = j - i + 1
-        total = sum(prefers[i:j+1])
+for count in range(k, n+1):
+    total = sum(prefers[:count])
+    square_total = sum(map(lambda x: x**2, prefers[:count]))
 
-        x = sum(map(lambda idx: (prefers[idx] * cnt - total) ** 2, range(i, j+1)))
-        mn = min(mn, x / (cnt * cnt * cnt))
+    start = 0
+    while 1:
+        var = count * square_total - 2 * (total ** 2) + (total ** 2)
+        mn = min(mn, var/(count ** 2))
+
+        nxt = start + count
+        if nxt >= n:
+            break
+
+        total += prefers[nxt] - prefers[start]
+        square_total += prefers[nxt] ** 2 - prefers[start] ** 2
+        start += 1
 
 print(sqrt(mn))
