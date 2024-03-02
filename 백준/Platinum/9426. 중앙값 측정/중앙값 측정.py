cd2294721f7f7655149e41b956787e655b572a44
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-03-02 18:29:16 +0900

    [Platinum V] Title: 중앙값 측정, Time: 3444 ms, Memory: 44300 KB -BaekjoonHub

--- 백준/Platinum/9426. 중앙값 측정/중앙값 측정.py
+++ 백준/Platinum/9426. 중앙값 측정/중앙값 측정.py
@@ -10,18 +10,18 @@ def update(idx, val, start, last, node):
     tree[node] += val
     if start < last:
         mid = start + last >> 1
-        update(idx, val, start, mid, 2*node+1)
-        update(idx, val, mid+1, last, 2*node+2)
+        update(idx, val, start, mid, node << 1)
+        update(idx, val, mid+1, last, (node << 1) + 1)
 
 
 def get_mid(node, rest):
-    if node >= MAX-1:
-        return node - MAX + 1
+    if node >= MAX:
+        return node - MAX
 
-    if tree[2*node+1] > rest:
-        return get_mid(2*node+1, rest)
+    if tree[node << 1] > rest:
+        return get_mid(node << 1, rest)
     else:
-        return get_mid(2*node+2, rest-tree[2*node+1])
+        return get_mid((node << 1) + 1, rest-tree[node << 1])
 
 
 n, k = map(int, input().split())
@@ -30,12 +30,12 @@ nums = [int(input()) for _ in range(n)]
 # 개수를 저장하는 segment tree
 tree = [0] * (MAX << 1)
 for num in nums[:k-1]:
-    update(num, 1, 0, MAX-1, 0)
+    update(num, 1, 0, MAX-1, 1)
 
 total = 0
 for i, num in enumerate(nums[k-1:]):
-    update(num, 1, 0, MAX-1, 0)
+    update(num, 1, 0, MAX-1, 1)
     total += get_mid(0, (k-1) >> 1)
-    update(nums[i], -1, 0, MAX-1, 0)
+    update(nums[i], -1, 0, MAX-1, 1)
 
 print(total)
