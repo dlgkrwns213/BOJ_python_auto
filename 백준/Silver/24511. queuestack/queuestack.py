c6e3da78242dff569778b8c8c371f977f49ded28
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-03-15 22:46:59 +0900

    [Silver III] Title: queuestack, Time: 140 ms, Memory: 48616 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.py
+++ 백준/Silver/24511. queuestack/queuestack.py
@@ -1,12 +1,10 @@
 n = int(input())
 A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-m = int(input())
 
-ans = []
-for i in range(n-1, -1, -1):
-    if not A[i]:
-        ans.append(B[i])
+ans = [b for i, b in enumerate(map(int, input().split())) if not A[i]]
+ans.reverse()
+
+m = int(input())
 ans += list(map(int, input().split()))
 
 print(' '.join(map(str, ans[:m])))
