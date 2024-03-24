a8c00912456e61c5be50f911dc028e798af21073
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-03-24 17:37:33 +0900

    [Gold III] Title: 하늘에서 별똥별이 빗발친다, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.py
@@ -0,0 +1,39 @@
+# https://www.acmicpc.net/problem/14658
+import sys
+input = sys.stdin.readline
+
+
+n, m, l, k = map(int, input().split())
+stars = sorted(list(map(int, input().split())) for _ in range(k))
+
+
+mx = 0
+for left, locations in enumerate(stars):
+    # x를 기준으로 가능한 y에 대해 모아줌
+    lx, ly = locations
+
+    y_locations = []
+    right = left
+    while right < k:
+        rx, ry = stars[right]
+        if rx - lx > l:
+            break
+
+        y_locations.append(ry)
+        right += 1
+
+    # y를 기준으로 막을 수 있는 최대 구하기
+    y_locations.sort()
+    cnt = len(y_locations)
+
+    up = 0
+    for down, y in enumerate(y_locations):
+        while up < cnt:
+            if y_locations[up] - y > l:
+                break
+
+            up += 1
+
+        mx = max(mx, up - down)
+
+print(k-mx)
