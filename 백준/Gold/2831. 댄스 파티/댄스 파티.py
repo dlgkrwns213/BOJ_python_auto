537f5835b138cc1ab3f71c30186b37da4499043a
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-03-25 11:41:33 +0900

    [Gold IV] Title: 댄스 파티, Time: 160 ms, Memory: 46264 KB -BaekjoonHub

---
+++ 백준/Gold/2831. 댄스 파티/댄스 파티.py
@@ -0,0 +1,33 @@
+# https://www.acmicpc.net/problem/2831
+def big_small_separate():
+    big, small = [], []
+    for x in map(int, input().split()):
+        if x > 0:
+            big.append(x)
+        else:
+            small.append(-x)
+
+    return sorted(big), sorted(small)
+
+
+def get_pair(big, small):
+    lenb, lens = len(big), len(small)
+    bi, si = 0, 0
+    cnt = 0
+
+    while bi < lenb and si < lens:
+        if big[bi] < small[si]:
+            cnt += 1
+            bi += 1
+            si += 1
+        else:
+            si += 1
+
+    return cnt
+
+
+input()
+bm, sm = big_small_separate()
+bw, sw = big_small_separate()
+
+print(get_pair(bm, sw) + get_pair(bw, sm))
