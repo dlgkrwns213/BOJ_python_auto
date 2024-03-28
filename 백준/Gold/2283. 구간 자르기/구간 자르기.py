f644f0916ea892168682e11eaa84816d9b58ce47
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-03-28 16:25:33 +0900

    [Gold II] Title: 구간 자르기, Time: 692 ms, Memory: 70676 KB -BaekjoonHub

---
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.py
@@ -0,0 +1,33 @@
+# https://www.acmicpc.net/problem/2283
+n, k = map(int, input().split())
+lines = [list(map(int, input().split())) for _ in range(n)]
+
+mx = max(map(lambda line: line[1], lines)) + 1
+
+# prefix sum
+prefix = [0] * mx
+for start, end in lines:
+    prefix[start] += 1
+    prefix[end] -= 1
+
+for i in range(1, mx):
+    prefix[i] += prefix[i-1]
+
+# two points
+left, right, now = 0, 0, 0
+possible = False
+while right < mx:
+    if now < k:
+        now += prefix[right]
+        right += 1
+    elif now > k:
+        now -= prefix[left]
+        left += 1
+    else:  # now == find
+        possible = True
+        break
+
+if possible:
+    print(left, right)
+else:
+    print(0, 0)
