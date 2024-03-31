51185b843dc8aaf5d5124930ccad98be813c49d3
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-03-31 15:43:50 +0900

    [Gold III] Title: A Great Way, Time: 56 ms, Memory: 33212 KB -BaekjoonHub

---
+++ 백준/Gold/16167. A Great Way/A Great Way.py
@@ -0,0 +1,44 @@
+# https://www.acmicpc.net/problem/16167
+import sys
+from heapq import heappop, heappush
+input = sys.stdin.readline
+INF = float('inf')
+
+
+def dijkstra():
+    distances = [INF] * (n+1)
+    distances[1] = 1
+
+    hq = []
+    heappush(hq, (1, 1))
+
+    while hq:
+        weight, now = heappop(hq)
+        if distances[now] < weight:
+            continue
+        dist, cnt = divmod(weight, 1000)
+        if now == n:
+            return dist, cnt
+
+        for nxt, nd in graph[now]:
+            nxt_dist = nd + dist
+            nxt_weight = (nxt_dist * 1000) + cnt + 1
+            if distances[nxt] > nxt_weight:
+                distances[nxt] = nxt_weight
+                heappush(hq, (nxt_weight, nxt))
+
+    return INF, INF
+
+
+n, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(r):
+    a, b, c, d, e = map(int, input().split())
+    w = c + (d * (e - 10) if e >= 10 else 0)
+    graph[a].append((b, w))
+
+total, count = dijkstra()
+if total == INF:
+    print('It is not a great way.')
+else:
+    print(total, count)
