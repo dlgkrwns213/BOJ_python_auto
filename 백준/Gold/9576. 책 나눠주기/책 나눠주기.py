ff40f44994eec7f04a83983dcdc27fdd7d0c9c94
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-04-03 15:33:15 +0900

    [Gold II] Title: 책 나눠주기, Time: 588 ms, Memory: 124760 KB -BaekjoonHub

---
+++ 백준/Gold/9576. 책 나눠주기/책 나눠주기.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+
+def dfs(now):
+    if visited[now]:
+        return False
+    visited[now] = True
+
+    a, b = book_range[now]
+    for book in range(a, b+1):
+        if matched[book] == -1 or dfs(matched[book]):
+            matched[book] = now
+            return True
+    return False
+
+
+ans = []
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    book_range = [tuple(map(int, input().split())) for _ in range(m)]
+
+    matched = [-1] * (n+1)
+    for i in range(m):
+        visited = [False] * m
+        dfs(i)
+
+    ans.append(n+1 - matched.count(-1))
+
+print('\n'.join(map(str, ans)))
