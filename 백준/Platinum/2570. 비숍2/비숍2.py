a93a04f9404c65156d88a712c834c09bb8d59fb5
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-04-06 16:41:31 +0900

    [Platinum II] Title: 비숍2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Platinum/2570. 비숍2/비숍2.py
@@ -0,0 +1,94 @@
+# https://www.acmicpc.net/problem/2570
+import sys
+input = sys.stdin.readline
+
+
+def get_graph(board):
+    ups, up_idx, use = [[-1]*n for _ in range(n)], 0, False
+    for i in range(n):
+        for j in range(i+1):
+            if board[i-j][j]:
+                ups[i-j][j] = up_idx
+                use = True
+            elif use:
+                up_idx += 1
+                use = False
+        if use:
+            up_idx += 1
+            use = False
+    for i in range(1, n):
+        for j in range(n-i):
+            if board[n-1-j][i+j]:
+                ups[n-1-j][i+j] = up_idx
+                use = True
+            elif use:
+                up_idx += 1
+                use = False
+        if use:
+            up_idx += 1
+            use = False
+
+    downs, down_idx, use = [[-1]*n for _ in range(n)], 0, False
+    for i in range(n-1, -1, -1):
+        for j in range(n-i):
+            if board[i+j][j]:
+                downs[i+j][j] = down_idx
+                use = True
+            elif use:
+                down_idx += 1
+                use = False
+        if use:
+            down_idx += 1
+            use = False
+
+    for i in range(1, n):
+        for j in range(n-i):
+            if board[j][i+j]:
+                downs[j][i+j] = down_idx
+                use = True
+            elif use:
+                down_idx += 1
+                use = False
+        if use:
+            down_idx += 1
+            use = False
+
+    graph = [set() for _ in range(up_idx)]
+    for i in range(n):
+        for j in range(n):
+            if board[i][j]:
+                graph[ups[i][j]].add(downs[i][j])
+
+    return up_idx, down_idx, graph
+
+
+def dfs(x):
+    if visited[x]:
+        return False
+    visited[x] = True
+
+    for y in graph[x]:
+        if matched[y] == -1:
+            matched[y] = x
+            return True
+    for y in graph[x]:
+        if dfs(matched[y]):
+            matched[y] = x
+            return True
+    return False
+
+
+n = int(input())
+board = [[1]*n for _ in range(n)]
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    board[a-1][b-1] = 0
+
+up, down, graph = get_graph(board)
+
+matched = [-1] * down
+for x in range(up):
+    visited = [False] * up
+    dfs(x)
+
+print(down - matched.count(-1))
