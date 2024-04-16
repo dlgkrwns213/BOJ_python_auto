b50f47ab584fae74aa9ddafada1bce5d78682d52
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-04-16 22:35:04 +0900

    [Gold V] Title: 2023년이 기대되는 이유, Time: 264 ms, Memory: 113124 KB -BaekjoonHub

---
+++ 백준/Gold/27231. 2023년이 기대되는 이유/2023년이 기대되는 이유.py
@@ -0,0 +1,49 @@
+# https://www.acmicpc.net/problem/27231
+import sys
+input = sys.stdin.readline
+
+
+def sum_kth_powers(k):
+    return sum(map(lambda num: num ** k, nums))
+
+
+def backtracking(idx):
+    global possible
+    if idx == n-1:
+        make_num, tmp = 0, 0
+        for i in range(n):
+            tmp *= 10
+            tmp += nums[i]
+            if location[i] == '+':
+                make_num += tmp
+                tmp = 0
+        make_num += tmp
+
+        if make_num in powers:
+            possible.add(make_num)
+        return
+
+    backtracking(idx+1)
+
+    location[idx] = '+'
+    backtracking(idx+1)
+    location[idx] = ''
+
+
+ans = []
+for _ in range(int(input())):
+    nums = list(map(int, input().rstrip()))
+    only_zeros_and_ones = all(num == 0 or num == 1 for num in nums)
+    if only_zeros_and_ones:
+        ans.append('Hello, BOJ 2023!')
+        continue
+
+    powers = set(sum_kth_powers(i) for i in range(1, 20))
+    n = len(nums)
+    location = [''] * n
+    possible = set()
+    backtracking(0)
+
+    ans.append(len(possible))
+
+print('\n'.join(map(str, ans)))
