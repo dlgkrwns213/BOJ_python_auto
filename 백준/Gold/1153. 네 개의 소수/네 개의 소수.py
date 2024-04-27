9fb6ca7d4b7e108784b203247a84ce19e4b57bb2
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-04-28 01:37:23 +0900

    [Gold III] Title: 네 개의 소수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1153. 네 개의 소수/네 개의 소수.py
+++ 백준/Gold/1153. 네 개의 소수/네 개의 소수.py
@@ -2,24 +2,17 @@
 MAX = int(1e6)
 
 
-def prime_checking():
-    is_prime = [True] * MAX
-    is_prime[0], is_prime[1] = False, False
-
-    for i in range(2, MAX):
-        if is_prime[i]:
-            for j in range(i+i, MAX, i):
-                is_prime[j] = False
-
-    return is_prime
+def is_prime(num):
+    for i in range(2, int(num ** 0.5) + 1):
+        if not num % i:
+            return False
+    return True
 
 
 n = int(input())
 if n < 8:
     print(-1)
 else:
-    is_prime = prime_checking()
-
     if n % 2:
         n -= 5
         ans = [2, 3]
@@ -28,7 +21,7 @@ else:
         ans = [2, 2]
 
     for i in range(2, MAX):
-        if is_prime[i] and is_prime[n-i]:
+        if is_prime(i) and is_prime(n-i):
             ans += [i, n-i]
             break
 
