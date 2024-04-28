18b2d27f87ee93765fc10858d2f8d3be059b6d65
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-04-28 21:25:38 +0900

    [Gold IV] Title: 좋은 친구, Time: 212 ms, Memory: 33660 KB -BaekjoonHub

--- 백준/Gold/3078. 좋은 친구/좋은 친구.py
+++ 백준/Gold/3078. 좋은 친구/좋은 친구.py
@@ -1,18 +1,17 @@
 # https://www.acmicpc.net/problem/3078
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-cnt, q, total = [0] * 21, deque(), 0
-for i in range(n):
-    length = len(input().rstrip())
+lengths = [len(input().rstrip()) for _ in range(n)]
+
+cnt, total = [0] * 21, 0
+for i, length in enumerate(lengths):
     total += cnt[length]
 
     if i >= k:
-        cnt[q.popleft()] -= 1
-
+        cnt[lengths[i-k]] -= 1
+        
     cnt[length] += 1
-    q.append(length)
 
 print(total)
