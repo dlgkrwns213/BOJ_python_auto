82fd88d76e33402ad1598d693f54507aa2a70784
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-05-10 11:23:58 +0900

    [Gold V] Title: 가희와 프로세스 1, Time: 2820 ms, Memory: 145548 KB -BaekjoonHub

---
+++ 백준/Gold/21773. 가희와 프로세스 1/가희와 프로세스 1.py
@@ -0,0 +1,26 @@
+# https://www.acmicpc.net/problem/21773
+import sys
+from heapq import heappop, heappush
+from collections import defaultdict
+input = sys.stdin.readline
+
+t, n = map(int, input().split())
+hq, rest = [], defaultdict(int)
+for _ in range(n):
+    ids, id_rest, prior = map(int, input().split())
+    heappush(hq, (-prior, ids))
+    rest[ids] = id_rest
+
+use = [-1] * t
+time = 0
+while time < t and hq:
+    prior, ids = heappop(hq)
+    use[time] = ids
+    rest[ids] -= 1
+
+    if rest[ids]:
+        heappush(hq, (prior+1, ids))
+
+    time += 1
+
+print('\n'.join(map(str, use[:time])))
