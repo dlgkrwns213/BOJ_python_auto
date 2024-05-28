195bd7bdd94591adfbcaeb4397c4ef1d414a98d3
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-05-29 00:18:52 +0900

    [Gold III] Title: 골목 대장 호석 - 효율성 1, Time: 3012 ms, Memory: 155316 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.py
+++ 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.py
@@ -6,33 +6,32 @@ INF = float('inf')
 MOD = (1 << 18) - 1
 
 
-def dijkstra(start, destination, init_money):
+def dijkstra(start, destination, init_cost, mx_money):
     costs = [INF] * (n+1)
     costs[start] = 0
     
     hq = []
     heappush(hq, (0, start))
-    
+
     while hq:
         cost, now = heappop(hq)
         if now == destination:
-            return cost >> 18
+            return True
         if costs[now] < cost:
             continue
 
-        mx = cost >> 18
-        total = cost & MOD
         for nxt, nm in graph[now]:
-            nxt_total = total + nm
-            if nxt_total > init_money:
+            if nm > mx_money:
                 continue
 
-            nxt_cost = (max(mx, nm) << 18) | nxt_total
+            nxt_cost = nm + cost
+            if nxt_cost > init_cost:
+                continue
             if costs[nxt] > nxt_cost:
                 costs[nxt] = nxt_cost
                 heappush(hq, (nxt_cost, nxt))
 
-    return -1
+    return False
 
 
 n, m, a, b, c = map(int, input().split())
@@ -41,5 +40,13 @@ for _ in range(m):
     u, v, w = map(int, input().split())
     graph[u].append((v, w))
     graph[v].append((u, w))
-    
-print(dijkstra(a, b, c))
+
+left, right = 1, 21
+while left < right:
+    mid = left + right >> 1
+    if dijkstra(a, b, c, mid):
+        right = mid
+    else:
+        left = mid + 1
+
+print(left if left != 21 else -1)
