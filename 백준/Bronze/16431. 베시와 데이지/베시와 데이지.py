23716b66fd9b7fe522032d6837b7866070a09fe1
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-06-02 18:13:12 +0900

    [Bronze III] Title: 베시와 데이지, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/16431. 베시와 데이지/베시와 데이지.py
@@ -0,0 +1,13 @@
+br, bc = map(int, input().split())
+dr, dc = map(int, input().split())
+jr, jc = map(int, input().split())
+
+bd = max(abs(br-jr), abs(bc-jc))
+dd = abs(dr-jr) + abs(dc-jc)
+
+if bd < dd:
+    print('bessie')
+elif bd == dd:
+    print('tie')
+else:
+    print('daisy')
