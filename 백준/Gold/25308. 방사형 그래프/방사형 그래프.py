887ae20d972a9be37a66657c039ce5d127f84830
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-06-05 09:35:59 +0900

    [Gold IV] Title: 방사형 그래프, Time: 200 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/25308. 방사형 그래프/방사형 그래프.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/25308\
+from itertools import permutations
+get_mn = lambda x, y: (2 ** 0.5) * x * y / (x + y)
+
+abilities = list(map(int, input().split()))
+
+cnt = 0
+for now in permutations(abilities, 8):
+    possible = 1
+    for i in range(8):
+        x, y, z = now[i], now[i-1], now[i-2]
+        if y < get_mn(x, z):
+            possible = 0
+            break
+
+    cnt += possible
+print(cnt)
