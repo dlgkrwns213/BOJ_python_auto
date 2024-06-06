b28518bf29cdcb2d29644b32e399a0e9ef4ac3d6
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-06-06 20:52:29 +0900

    [Gold IV] Title: 성싶당 밀키트, Time: 2872 ms, Memory: 63480 KB -BaekjoonHub

---
+++ 백준/Gold/24041. 성싶당 밀키트/성싶당 밀키트.py
@@ -0,0 +1,32 @@
+# https://www.acmicpc.net/problem/24041
+import sys
+input = sys.stdin.readline
+
+
+def get_bacterias(day):
+    bacteria = sum(map(lambda ingredient: ingredient[0] * max(1, day-ingredient[1]), imp))
+    if uim_count > k:
+        uim_bacteria = sum(sorted(map(lambda ingredient: ingredient[0] * max(1, day-ingredient[1]), uim))[:uim_count-k])
+        bacteria += uim_bacteria
+    return bacteria
+
+
+n, g, k = map(int, input().split())
+uim, imp = [], []
+for _ in range(n):
+    s, l, o = map(int, input().split())
+    if o:
+        uim.append((s, l))
+    else:
+        imp.append((s, l))
+
+uim_count = len(uim)
+left, right = 1, int(1e10)
+while left < right:
+    mid = left + right >> 1
+    if get_bacterias(mid) > g:
+        right = mid
+    else:
+        left = mid + 1
+
+print(left-1)
