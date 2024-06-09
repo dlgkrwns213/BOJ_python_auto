0a9bcd82cafd2476c4b24c6b46d5cefaa2487759
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-06-09 16:30:50 +0900

    [Gold IV] Title: 일감호에 다리 놓기, Time: 1908 ms, Memory: 157488 KB -BaekjoonHub

--- 백준/Gold/17490. 일감호에 다리 놓기/일감호에 다리 놓기.py
+++ 백준/Gold/17490. 일감호에 다리 놓기/일감호에 다리 놓기.py
@@ -3,21 +3,23 @@ import sys
 input = sys.stdin.readline
 
 n, m, k = map(int, input().split())
+
 if m <= 1:
-    print('YES')
-    exit(0)
+    ans = 'YES'
+else:
+    s = list(map(int, input().split())) * 2
 
-s = list(map(int, input().split())) * 2
+    boundaries = []
+    for _ in range(m):
+        i, j = map(int, input().split())
+        if i > j:
+            i, j = j, i
 
-boundaries = []
-for _ in range(m):
-    i, j = map(int, input().split())
-    if i > j:
-        i, j = j, i
+        boundaries.append(j if i == 1 and j == n else i)
+    boundaries.sort()
+    boundaries.append(boundaries[0]+n)
 
-    boundaries.append(j if i == 1 and j == n else i)
-boundaries.sort()
-boundaries.append(boundaries[0]+n)
+    total = sum(map(lambda idx: min(s[boundaries[idx]:boundaries[idx+1]]), range(m)))
+    ans = 'YES' if total <= k else 'NO'
 
-total = sum(map(lambda idx: min(s[boundaries[idx]:boundaries[idx+1]]), range(m)))
-print('YES' if total <= k else 'NO')
+print(ans)
