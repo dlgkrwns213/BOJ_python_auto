7d6283e657ac972619830afe44e8254b097ef67a
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-07-01 00:06:11 +0900

    [Silver II] Title: 서버실, Time: 4156 ms, Memory: 71112 KB -BaekjoonHub

---
+++ 백준/Silver/17245. 서버실/서버실.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+
+def turnon(height):
+    return sum(map(lambda computer: min(height, computer), computers))
+
+
+n = int(input())
+computers = [x for _ in range(n) for x in map(int, input().split())]
+
+half = sum(computers)+1 >> 1
+left, right = 0, int(1e7) + 1
+while left < right:
+    mid = left + right >> 1
+    if turnon(mid) >= half:
+        right = mid
+    else:
+        left = mid + 1
+
+print(left)
