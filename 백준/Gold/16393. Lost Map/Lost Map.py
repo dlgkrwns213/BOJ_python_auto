1bff3b3f45cc5ce52a9e88c5ed5d2d142447764a
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-09-03 17:26:42 +0900

    [Gold II] Title: Lost Map, Time: 6036 ms, Memory: 496784 KB -BaekjoonHub

---
+++ 백준/Gold/16393. Lost Map/Lost Map.py
@@ -0,0 +1,39 @@
+# https://www.acmicpc.net/problem/16393
+import sys
+input = sys.stdin.readline
+
+
+def find(x):
+    if x == parent[x]:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+
+def union(x, y):
+    x, y = map(find, (x, y))
+
+    if rank[x] >= rank[y]:
+        rank[x] += rank[y]
+        parent[y] = x
+    else:
+        rank[y] += rank[x]
+        parent[x] = y
+
+
+n = int(input())
+distances = []
+for i in range(n):
+    for j, v in enumerate(map(int, input().split()[:i])):
+        distances.append((j, i, v))
+distances.sort(key=lambda distance: distance[2])
+
+parent, rank = list(range(n)), [1] * n
+links = []
+for i, j, v in distances:
+    if find(i) != find(j):
+        union(i, j)
+        links.append((i+1, j+1))
+links.sort()
+
+print('\n'.join(map(lambda line: ' '.join(map(str, line)), links)))
