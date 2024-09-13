6aa07e981ddb67bbb19d5fdba534b68d71f6d12e
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-09-13 23:26:38 +0900

    [Bronze III] Title: 경고, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3029. 경고/경고.py
+++ 백준/Bronze/3029. 경고/경고.py
@@ -5,4 +5,4 @@ gap_second = (3600*(hf-hs) + 60*(mf-ms) + (sf-ss)) % 86400 or 86400
 gap_second, s = divmod(gap_second, 60)
 h, m = divmod(gap_second, 60)
 
-print(':'.join(map(lambda x: f'0{x}'[-2:], (h, m, s))))
+print(f'{h:02}:{m:02}:{s:02}')
