cdb8dc2053a7544a20cee080f22f0ca3bd5877b9
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-09-13 23:34:56 +0900

    [Bronze III] Title: 경고, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3029. 경고/경고.py
+++ 백준/Bronze/3029. 경고/경고.py
@@ -1,8 +1,11 @@
-hs, ms, ss = map(int, input().split(':'))
-hf, mf, sf = map(int, input().split(':'))
+s = map(int, input().split(':'))
 
-gap_second = (3600*(hf-hs) + 60*(mf-ms) + (sf-ss)) % 86400 or 86400
-gap_second, s = divmod(gap_second, 60)
-h, m = divmod(gap_second, 60)
+t = 0
+for f in map(int, input().split(':')):
+    t *= 60
+    t += f-next(s)
+t = t % 86400 or 86400
 
+t, s = divmod(t, 60)
+h, m = divmod(t, 60)
 print(f'{h:02}:{m:02}:{s:02}')
