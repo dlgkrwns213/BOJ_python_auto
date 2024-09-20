ac089f54c71fb715026a4e4a7c60a6d514c5d5a8
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-09-20 23:09:32 +0900

    [Bronze III] Title: 이게 분수?, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2863. 이게 분수？/이게 분수？.py
+++ 백준/Bronze/2863. 이게 분수？/이게 분수？.py
@@ -1,4 +1,4 @@
 a,b=map(int,input().split())
 c,d=map(int,input().split())
 x=a/c+b/d,c/d+a/b,d/b+c/a,b/a+d/c
-print(next(i for i,v in enumerate(x)if v==max(x)))
+print(next(i for i in range(4)if x[i]==max(x)))
