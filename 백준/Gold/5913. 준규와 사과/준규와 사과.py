a763274ad3bb1fdaaa9d78ca14d816c8be325f99
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-10-02 12:30:47 +0900

    [Gold IV] Title: 준규와 사과, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/5913. 준규와 사과/준규와 사과.py
+++ 백준/Gold/5913. 준규와 사과/준규와 사과.py
@@ -6,24 +6,6 @@ go = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
 def backtracking(rest, jx, jy, hx, hy):
     global cnt
-    if not rest:
-        for a, b in go:
-            njx, njy = jx + a, jy + b
-            if njx < 0 or njx >= 5 or njy < 0 or njy >= 5:
-                continue
-            if board[njx][njy]:
-                continue
-
-            for c, d in go:
-                nhx, nhy = hx+c, hy+d
-                if nhx < 0 or nhx >= 5 or nhy < 0 or nhy >= 5:
-                    continue
-                if board[nhx][nhy]:
-                    continue
-
-                if (njx, njy) == (nhx, nhy):
-                    cnt += 1
-        return
 
     for a, b in go:
         njx, njy = jx+a, jy+b
@@ -32,7 +14,6 @@ def backtracking(rest, jx, jy, hx, hy):
         if board[njx][njy]:
             continue
 
-        board[njx][njy] = 1
         for c, d in go:
             nhx, nhy = hx+c, hy+d
             if nhx < 0 or nhx >= 5 or nhy < 0 or nhy >= 5:
@@ -40,10 +21,16 @@ def backtracking(rest, jx, jy, hx, hy):
             if board[nhx][nhy]:
                 continue
 
+            if (nhx, nhy) == (njx, njy):
+                if not rest:
+                    cnt += 1
+                continue
+
+            board[njx][njy] = 1
             board[nhx][nhy] = 1
             backtracking(rest-1, njx, njy, nhx, nhy)
             board[nhx][nhy] = 0
-        board[njx][njy] = 0
+            board[njx][njy] = 0
 
 
 board = [[0]*5 for _ in range(5)]
