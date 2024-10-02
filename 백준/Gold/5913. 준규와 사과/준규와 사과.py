7422c75a0d567764ab834baa73dc7dabb1967a10
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-10-02 12:24:35 +0900

    [Gold IV] Title: 준규와 사과, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/5913. 준규와 사과/준규와 사과.py
@@ -0,0 +1,58 @@
+# https://www.acmicpc.net/problem/5913
+import sys
+input = sys.stdin.readline
+go = ((-1, 0), (1, 0), (0, -1), (0, 1))
+
+
+def backtracking(rest, jx, jy, hx, hy):
+    global cnt
+    if not rest:
+        for a, b in go:
+            njx, njy = jx + a, jy + b
+            if njx < 0 or njx >= 5 or njy < 0 or njy >= 5:
+                continue
+            if board[njx][njy]:
+                continue
+
+            for c, d in go:
+                nhx, nhy = hx+c, hy+d
+                if nhx < 0 or nhx >= 5 or nhy < 0 or nhy >= 5:
+                    continue
+                if board[nhx][nhy]:
+                    continue
+
+                if (njx, njy) == (nhx, nhy):
+                    cnt += 1
+        return
+
+    for a, b in go:
+        njx, njy = jx+a, jy+b
+        if njx < 0 or njx >= 5 or njy < 0 or njy >= 5:
+            continue
+        if board[njx][njy]:
+            continue
+
+        board[njx][njy] = 1
+        for c, d in go:
+            nhx, nhy = hx+c, hy+d
+            if nhx < 0 or nhx >= 5 or nhy < 0 or nhy >= 5:
+                continue
+            if board[nhx][nhy]:
+                continue
+
+            board[nhx][nhy] = 1
+            backtracking(rest-1, njx, njy, nhx, nhy)
+            board[nhx][nhy] = 0
+        board[njx][njy] = 0
+
+
+board = [[0]*5 for _ in range(5)]
+k = int(input())
+for _ in range(k):
+    i, j = map(lambda _: int(_)-1, input().split())
+    board[i][j] = 1
+board[0][0], board[4][4] = 1, 1
+
+cnt = 0
+backtracking(11-k//2, 0, 0, 4, 4)
+print(cnt)
