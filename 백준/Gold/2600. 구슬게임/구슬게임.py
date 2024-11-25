971c3b475f29651f6ce8d89756d865ffeb26609d
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-11-25 14:06:39 +0900

    [Gold IV] Title: 구슬게임, Time: 212 ms, Memory: 32572 KB -BaekjoonHub

--- 백준/Gold/2600. 구슬게임/구슬게임.py
+++ 백준/Gold/2600. 구슬게임/구슬게임.py
@@ -1,13 +1,12 @@
 # https://www.acmicpc.net/problem/2600
 b1, b2, b3 = list(map(int, input().split()))
+wins = [[False] * 531 for _ in range(531)]
+for i in range(501):
+    for j in range(501):
+        enemy_lose = not any((wins[i-b1][j], wins[i-b2][j], wins[i-b3][j],
+                              wins[i][j-b1], wins[i][j-b2], wins[i][j-b3]))
+        wins[i][j] = enemy_lose
+
 for _ in range(5):
     k1, k2 = map(int, input().split())
-
-    wins = [[0]*(k2+31) for _ in range(k1+31)]
-    for i in range(k1+1):
-        for j in range(k2+1):
-            enemy_lose = not any((wins[i-b1][j], wins[i-b2][j], wins[i-b3][j],
-                                  wins[i][j-b1], wins[i][j-b2], wins[i][j-b3]))
-            wins[i][j] = enemy_lose
-
     print('B' if wins[k1][k2] else 'A')
