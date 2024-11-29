79c001e4fcdb48204d9d8ee45eb29747c0b81ece
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2024-11-30 00:01:30 +0900

    [Bronze I] Title: 차량 번호판 1, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/16968. 차량 번호판 1/차량 번호판 1.py
+++ 백준/Bronze/16968. 차량 번호판 1/차량 번호판 1.py
@@ -1,13 +1,10 @@
 bef, cnt = '', 0
 total = 1
+change = {'d': 10, 'c': 26}
 for x in input():
     if bef != x:
         if cnt:
-            if bef == 'd':
-                total *= 10 * (9 ** (cnt-1))
-            else:
-                total *= 26 * (25 ** (cnt-1))
-
+            total *= (10 * (9 ** (cnt-1))) if bef == 'd' else (26 * (25 ** (cnt-1)))
         bef, cnt = x, 1
     else:
         cnt += 1
