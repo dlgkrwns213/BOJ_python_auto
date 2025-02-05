ea011badb1373498f064e594f357f54b378c6a21
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-02-06 00:46:23 +0900

    [Bronze II] Title: 박사 과정, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5026. 박사 과정/박사 과정.py
+++ 백준/Bronze/5026. 박사 과정/박사 과정.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 for _ in range(int(input())):
-    ipt = input()
+    ipt = input().rstrip()
     print(sum(map(int, ipt.split('+'))) if '+' in ipt else 'skipped')
