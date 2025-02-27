5153705a29ac55208c18961bf65bdb62183bb22d
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-02-27 19:37:34 +0900

    [Silver I] Title: 정수 삼각형, Time: 256 ms, Memory: 27516 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,25 +1,32 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = scanner.nextInt();
+        int n = Integer.parseInt(br.readLine());
         int[][] numbers = new int[n][n];
-        for (int i=0;i<n;i++) {
-            for (int j=0;j<=i;j++) {
-                numbers[i][j] = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <= i; j++) {
+                numbers[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         int[][] dp = new int[n][n];
         dp[0][0] = numbers[0][0];
-        for (int i=1;i<n;i++) {
-            for (int j=0;j<=i;j++)
-                dp[i][j] = Math.max(j>0 ? dp[i-1][j-1] : 0, dp[i-1][j]) + numbers[i][j];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = Math.max(j > 0 ? dp[i - 1][j - 1] : 0, dp[i - 1][j]) + numbers[i][j];
+            }
         }
 
-        System.out.println(Arrays.stream(dp[n-1]).max().orElse(-1));
+        int maxValue = Arrays.stream(dp[n - 1]).max().orElse(-1);
+        bw.write(String.valueOf(maxValue) + "\n");
+        bw.flush();
     }
 }
