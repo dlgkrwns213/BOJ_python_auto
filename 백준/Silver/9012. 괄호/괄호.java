2a948dfa730ca535d63d35eef162ead1f33fcd04
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-02-28 01:40:11 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,28 +1,34 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = scanner.nextInt();
-        for (int t=0;t<n;t++) {
-            String s = scanner.next();
+        int n = Integer.parseInt(br.readLine());
+        for (int t = 0; t < n; t++) {
+            String s = br.readLine();
+            int count = 0;
             boolean possible = true;
 
-            int count = 0;
-            for (int i=0;i<s.length();i++) {
+            for (int i = 0; i < s.length(); i++) {
                 char p = s.charAt(i);
-                count += s.charAt(i) == '(' ? 1 : -1;
+                count += (p == '(') ? 1 : -1;
                 if (count < 0) {
                     possible = false;
                     break;
                 }
             }
 
-            if (count != 0)
+            if (count != 0) {
                 possible = false;
+            }
 
-            System.out.println(possible ? "YES" : "NO");
+            bw.write(possible ? "YES\n" : "NO\n");
         }
+
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
