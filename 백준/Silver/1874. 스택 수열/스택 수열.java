a09b02cc6428214f859ce774160d19837fa84b9a
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-04-02 16:17:32 +0900

    [Silver II] Title: 스택 수열, Time: 432 ms, Memory: 44980 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+
+        StringBuilder ans = new StringBuilder();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int use = 1;
+        boolean possible = true;
+
+        for (int unused = 0; unused < n; unused++) {
+            int num = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+            while (use <= num) {
+                stack.push(use++);
+                ans.append('+');
+            }
+
+            if (stack.pop() == num) {
+                ans.append('-');
+            } else {
+                possible = false;
+                break;
+            }
+        }
+
+        System.out.println(possible ?
+                ans.chars()
+                        .mapToObj(c -> String.valueOf((char)c))
+                        .collect(Collectors.joining("\n"))
+                : "NO");
+    }
+}
