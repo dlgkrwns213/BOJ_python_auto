6b45993347350e10fe75313e2f9c4437e837cfc1
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-04-09 18:52:55 +0900

    [Silver V] Title: 귀찮음, Time: 96 ms, Memory: 14200 KB, Score: 4 point -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int unused = 0; unused < n; unused++)
+            heap.add(Integer.parseInt(st.nextToken()));
+
+        long total = 0L;
+        for (int unused = 0; unused < n-1; unused++){
+            int one = heap.poll();
+            int two = heap.poll();
+
+            total += one * two;
+            heap.add(one+two);
+        }
+
+        System.out.println(total);
+    }
+}
