13cd0c3ca2fe46ebeb835dab666b69349a501484
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-04-16 15:41:14 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 696 ms, Memory: 82080 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -3,16 +3,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        final int MOD1 = 1_000_000_007;
+        final int MOD2 = 1_000_000_009;
+        final int BASE = 27;
+
         Scanner sc = new Scanner(System.in);
+        String str = sc.next();
 
         HashSet<String> set = new HashSet<>();
-        String string = sc.next();
 
-        for (int i = 0; i < string.length(); i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = i; j < string.length(); j++) {
-                sb.append(string.charAt(j));
-                set.add(sb.toString());
+        for (int i = 0; i < str.length(); i++) {
+            long h1 = 0;
+            long h2 = 0;
+            for (int j = i; j < str.length(); j++) {
+                int c = str.charAt(j) - 'a' + 1;
+
+                h1 = (h1 * BASE + c) % MOD1;
+                h2 = (h2 * BASE + c) % MOD2;
+
+                set.add(h1 + "," + h2);
             }
         }
 
