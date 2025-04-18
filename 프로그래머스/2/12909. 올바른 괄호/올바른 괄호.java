6964ab6ba665875d682a9268dbccaec632ba4879
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-04-18 13:38:32 +0900

    [level 2] Title: 올바른 괄호, Time: 6.33 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,15 +1,10 @@
 class Solution {
     boolean solution(String s) {
-        int openCount = 0;
-        for (char c: s.toCharArray()) {
-            if (c == '(')
-                openCount++;
-            else
-                openCount--;
-            
-            if (openCount < 0)
-                return false;
-        }
-        return openCount == 0 ? true : false;
+        boolean answer = true;
+
+        for (char c: s.toCharArray())
+            System.out.println(c);
+
+        return answer;
     }
 }
