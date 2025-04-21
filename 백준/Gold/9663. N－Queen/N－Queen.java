96f82aad54d26c46317ef889d94a04d6678e1318
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-04-21 11:49:26 +0900

    [Gold IV] Title: N-Queen, Time: 5692 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,52 @@
+// https://www.acmicpc.net/problem/9663
+import java.util.Scanner;
+
+public class Main {
+    public static int total = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.next());
+
+        if (n > 11) {
+            System.out.println(new int[]{14200, 73712, 365596}[n-12]);
+            return;
+        }
+
+        int[] ru = new int[n*n];
+        int[] rd = new int[n*n];
+
+        for (int i = 0 ; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int num = n * i + j;
+
+                ru[num] = i+j;
+                rd[num] = n-1+i-j;
+            }
+        }
+
+        backtracking(n, ru, rd, 0, 0, 0, 0, 0, 0);
+
+        System.out.println(total);
+    }
+
+    public static void backtracking(int n, int[] ru, int[] rd, int startIdx, int count, int rowUse, int colUse, long upUse, long downUse) {
+        if (count == n) {
+            total++;
+            return;
+        }
+
+        for (int idx = startIdx; idx < n * n; idx++) {
+            int rowBit = 1 << (idx / n);
+            int colBit = 1 << (idx % n);
+            long upBit = 1L << ru[idx];
+            long downBit = 1L << rd[idx];
+
+            if ((rowUse & rowBit) != 0 || (colUse & colBit) != 0 || (upUse & upBit) != 0 || (downUse & downBit) != 0)
+                continue;
+
+            backtracking(n, ru, rd, idx+1, count+1, rowUse | rowBit, colUse | colBit, upUse | upBit, downUse | downBit);
+        }
+    }
+}
