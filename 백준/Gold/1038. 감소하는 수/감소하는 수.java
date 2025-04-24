b218558e136bd5b14a2c507decc8fd9ab003fe6f
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-04-24 19:36:14 +0900

    [Gold V] Title: 감소하는 수, Time: 180 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static ArrayList<Long> possibles = new ArrayList<>();
+    static int ans = -1;
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+
+        for (Long start = 0L; start < 10; start++)
+            backtracking(start);
+
+        possibles.sort(Long::compareTo);
+        System.out.println(n < possibles.size() ? possibles.get(n) : -1);
+    }
+
+    public static void backtracking(Long make) {
+        possibles.add(make);
+
+        for (int i = 0; i < make % 10; i++)
+            backtracking(make * 10 + i);
+    }
+}
