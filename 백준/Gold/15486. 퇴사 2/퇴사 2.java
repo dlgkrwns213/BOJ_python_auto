c1743f7641f3d22af8c4b44c730f86aeea759006
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-05-07 11:23:48 +0900

    [Gold V] Title: 퇴사 2, Time: 1208 ms, Memory: 376296 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -17,12 +17,12 @@ public class Main {
 
         int[] dp = new int[n+1];
         for (int day = 0; day < n; day++) {
-            dp[day] = Math.max(dp[day], (day > 1 ? dp[day-1] : 0));
+            dp[day] = Math.max(dp[day], (day > 1 ? dp[day-1] : 0));  // 오늘까지의 최대값을 오늘로 저장
             int t = days[day][0];
             int p = days[day][1];
 
-            if (day + t <= n)
-                dp[day+t] = Math.max(dp[day+t], dp[day] + p);
+            if (day + t <= n)  // 오늘 일을 시작했을 경우의 값을 미리 설정
+                dp[day+t] = Math.max(dp[day+t], dp[day] + p); 
         }
 
         System.out.println(Arrays.stream(dp).max().orElse(0));
