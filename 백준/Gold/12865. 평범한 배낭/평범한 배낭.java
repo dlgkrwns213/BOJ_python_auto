62c572624e52fcd3ad180918b70a8a7054730e4b
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-05-10 15:37:44 +0900

    [Gold V] Title: 평범한 배낭, Time: 176 ms, Memory: 54984 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
@@ -13,26 +12,23 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[][] things = new int[n][2];
-        for (int i = 0; i < n; i++)
-            things[i] = Arrays.stream(br.readLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-
-        int[][] dp = new int[n+1][k+1];
+        int[] now = new int[k+1];
         for (int i = 0; i < n; i++) {
-            int nowWeight = things[i][0];
-            int nowValue = things[i][1];
+            st = new StringTokenizer(br.readLine());
+            int nowWeight = Integer.parseInt(st.nextToken());
+            int nowValue = Integer.parseInt(st.nextToken());
 
+            int[] nxt = new int[k+1];
             for (int w = 0; w <= k; w++) {
-                if (w < nowWeight) {
-                    dp[i+1][w] = dp[i][w];
-                } else {
-                    dp[i+1][w] = Math.max(dp[i][w], dp[i][w-nowWeight] + nowValue);
-                }
+                if (w < nowWeight)
+                    nxt[w] = now[w];
+                else
+                    nxt[w] = Math.max(now[w], now[w-nowWeight] + nowValue);
             }
+
+            now = nxt;
         }
 
-        System.out.println(dp[n][k]);
+        System.out.println(now[k]);
     }
 }
