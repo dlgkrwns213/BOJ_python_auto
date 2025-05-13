475ad35486c736bd6d5025ee0a7cefff4e21a72e
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-05-13 16:46:40 +0900

    [Gold IV] Title: 부분합, Time: 276 ms, Memory: 26476 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        int[] nums = Arrays.stream((br.readLine() + " 0").split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int left = 0, right = 0;
+        int now = 0, ans = n+1;
+        while (right <= n) {
+            if (now < s) {
+                now += nums[right++];
+            } else {
+                ans = Math.min(ans, right-left);
+                now -= nums[left++];
+            }
+        }
+
+        System.out.println(ans != n+1 ? ans : 0);
+    }
+}
