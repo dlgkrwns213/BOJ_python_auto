e14478a0e100f7984286ad774d6ecc8556dadba5
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-05-15 16:42:47 +0900

    [Silver I] Title: 싫은데요, Time: 388 ms, Memory: 66608 KB -BaekjoonHub

--- 백준/Silver/25916. 싫은데요/싫은데요.java
+++ 백준/Silver/25916. 싫은데요/싫은데요.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,12 +11,15 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[] a = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        int[] a = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
 
         int left = 0, right = 0;
         int mx = 0, v = 0;
+
         while (right < n) {
             v += a[right++];
 
