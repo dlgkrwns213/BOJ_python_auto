2a572f37699ec5d07589c859bb1f8ab8179d8c3d
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-05-20 11:38:31 +0900

    [Gold V] Title: 근성아 일하자, Time: 212 ms, Memory: 125200 KB -BaekjoonHub

--- 백준/Gold/32358. 근성아 일하자/근성아 일하자.py
+++ 백준/Gold/32358. 근성아 일하자/근성아 일하자.py
@@ -3,31 +3,30 @@ import sys
 input = sys.stdin.readline
 INF = int(1e9)
 
-wastes = {-INF, INF}
+wastes = [-INF, INF]
 now, total = 0, 0
 for _ in range(int(input())):
     q, *x = map(int, input().split())
     if q == 1:
-        wastes.add(x[0])
+        wastes.append(x[0])
         continue
 
-    sorted_wastes = sorted(wastes)
+    wastes.sort()
     left, right = 0, 0
-    for i in range(len(sorted_wastes)-1):
-        if sorted_wastes[i] <= now <= sorted_wastes[i+1]:
+    for i in range(len(wastes)-1):
+        if wastes[i] <= now <= wastes[i+1]:
             left, right = i, i+1
-            break
 
-    for _ in range(len(sorted_wastes)-2):
-        if now - sorted_wastes[left] <= sorted_wastes[right] - now:
-            total += now - sorted_wastes[left]
-            now = sorted_wastes[left]
+    for _ in range(len(wastes)-2):
+        if now - wastes[left] <= wastes[right] - now:
+            total += now - wastes[left]
+            now = wastes[left]
             left -= 1
         else:
-            total += sorted_wastes[right] - now
-            now = sorted_wastes[right]
+            total += wastes[right] - now
+            now = wastes[right]
             right += 1
 
-    wastes = {-INF, INF}
+    wastes = [-INF, INF]
 
 print(total)
