5c9d6d2317fbed2d200f665c2b7143b32dca68ca
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-05-23 16:35:11 +0900

    [Bronze III] Title: 스트릿 코딩 파이터, Time: 112 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/23348. 스트릿 코딩 파이터/스트릿 코딩 파이터.py
+++ 백준/Bronze/23348. 스트릿 코딩 파이터/스트릿 코딩 파이터.py
@@ -1,8 +1,2 @@
 scores = list(map(int, input().split()))
-mx = 0
-for _ in range(int(input())):
-    x = sum([list(map(int, input().split())) for _ in range(3)], [])
-    total = sum(map(lambda idx: scores[idx%3] * x[idx], range(9)))
-
-    mx = max(mx, total)
-print(mx)
+print(max(sum(map(lambda idx: scores[idx % 3] * x[idx], range(9))) for _ in range(int(input())) for x in [[*sum([list(map(int, input().split())) for _ in range(3)], [])]]))
