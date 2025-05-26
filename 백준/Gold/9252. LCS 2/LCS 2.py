1b8481356f50aa76bcf31652b75710c1ceaa26db
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-05-26 17:18:48 +0900

    [Gold IV] Title: LCS 2, Time: 224 ms, Memory: 56604 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -0,0 +1,32 @@
+# https://www.acmicpc.net/problem/30805
+def get_lcs() -> str:
+    n, m = len(a), len(b)
+    dp = [[0] * (m+1) for _ in range(n+1)]
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if a[i-1] == b[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+    reversed_lcs = []
+    i, j = n, m
+    while i and j:
+        if a[i-1] == b[j-1]:
+            reversed_lcs.append(a[i-1])
+            i -= 1
+            j -= 1
+        elif dp[i-1][j] >= dp[i][j-1]:
+            i -= 1
+        else:
+            j -= 1
+
+    return ''.join(reversed(reversed_lcs))
+
+
+a = input()
+b = input()
+
+lcs = get_lcs()
+print(len(lcs))
+print(lcs)
