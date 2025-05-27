777d5208aa487546322ca08dcc8b97a97638eac5
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-05-27 09:55:06 +0900

    [Silver III] Title: N번째 큰 수, Time: 836 ms, Memory: 249484 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 
 public class Main {
@@ -9,15 +10,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] numbers = new int[n*n];
-        int idx = 0;
-        for (int unused = 0; unused < n; unused++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            while (st.hasMoreTokens())
-                numbers[idx++] = Integer.parseInt(st.nextToken());
+        // 가장 큰 n개 수를 저장하는 최소 힙
+        PriorityQueue<Integer> maxNNumbers = new PriorityQueue<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 기존에 N개 저장
+        while (st.hasMoreTokens())
+            maxNNumbers.add(Integer.valueOf(st.nextToken()));  // valueOf로 boxing 한 채로 저장
+
+        for (int unused = 1; unused < n; unused++) {
+            st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()) {
+                Integer number = Integer.valueOf(st.nextToken());
+                // 우선순위 큐에 존재하는 가장 작은 값보다 큰 경우에만 넣어주면서 가장 큰 N개만 유지
+                if (maxNNumbers.peek() < number) {
+                    maxNNumbers.poll();
+                    maxNNumbers.add(number);
+                }
+            }
         }
 
-        Arrays.sort(numbers);
-        System.out.println(numbers[numbers.length-n]);
+        System.out.println(maxNNumbers.peek());
     }
 }
