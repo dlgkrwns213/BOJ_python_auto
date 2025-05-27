d94b73fdbdfd5cfb5ad5ba5f0f7d6096b38b0e66
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-05-27 10:26:46 +0900

    [Gold V] Title: 탑, Time: 660 ms, Memory: 88592 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,26 +1,28 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
         ArrayDeque<int[]> stack = new ArrayDeque<>();
         stack.push(new int[]{Integer.MAX_VALUE, 0});
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        StringBuilder ans = new StringBuilder();
         for (int idx = 1; idx <= n; idx++) {
             int height = Integer.parseInt(st.nextToken());
 
             while (stack.peek()[0] < height)
                 stack.pop();
 
-            bw.write(stack.peek()[1] + " ");
+            ans.append(stack.peek()[1]).append(' ');
             stack.push(new int[]{height, idx});
         }
-        bw.flush();
+        System.out.println(ans);
     }
 }
