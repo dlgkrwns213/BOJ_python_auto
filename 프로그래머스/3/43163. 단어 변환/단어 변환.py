382b533a0ec64b9d7881523673a86adec405e625
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-05-30 13:43:36 +0900

    [level 3] Title: 단어 변환, Time: 1.84 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,40 +1,38 @@
 from collections import deque
 
-
 def solution(begin, target, words):
     if target not in words:
         return 0
-    words += [target, begin]
-    n = len(words)
-    size = len(words[0])
     
+    words.append(begin)
+    idx = len(words[0])
+    n = len(words)
     graph = [[] for _ in range(n)]
-    for i, vi in enumerate(words):
-        for j, vj in enumerate(words[:i]):
-            count = sum(map(lambda idx: 1 if vi[idx] != vj[idx] else 0, range(size)))
-            if count == 1:
+    for i, wi in enumerate(words):
+        for j, wj in enumerate(words[:i]):
+            if sum(map(lambda i: 1 if wi[i] != wj[i] else 0, range(idx))) == 1:
                 graph[i].append(j)
                 graph[j].append(i)
                 
-    print(graph)
-    return bfs(graph, n)
-
+    return bfs(graph, n, n-1, words.index(target))
     
-def bfs(graph, n):
-    visited = [False] * n
-    visited[n-1] = True
+
+def bfs(graph, n, start, destination):
+    visited = [0] * n
+    visited[start] = 1
     
     q = deque()
-    q.append((n-1, 0))
+    q.append((start, 0))
     
     while q:
         now, count = q.popleft()
-        if now == n-2:
+        if now == destination:
             return count
         
         for nxt in graph[now]:
             if not visited[nxt]:
-                visited[nxt] = True
+                visited[nxt] = 1
                 q.append((nxt, count+1))
                 
-    return 0
+    return 0            
+    
