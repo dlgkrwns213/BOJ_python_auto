dbd3a0009f977276f5204e34da140b96d12db7da
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-06-13 17:21:57 +0900

    [Bronze I] Title: 최소공배수, Time: 116 ms, Memory: 14536 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder answer = new StringBuilder();
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int gcd = getGcd(a, b);
+            answer.append(a / gcd * b).append('\n');
+        }
+        System.out.println(answer);
+    }
+
+    private static int getGcd(int a, int b) {
+        while (b > 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
