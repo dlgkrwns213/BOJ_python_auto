8941ffdeca9c31222e73cff3f5715a13dc5526d6
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-06-21 16:52:14 +0900

    [Gold V] Title: 파괴된 도시, Time: 168 ms, Memory: 39888 KB -BaekjoonHub

---
+++ 백준/Gold/18231. 파괴된 도시/파괴된 도시.py
@@ -0,0 +1,31 @@
+# https://www.acmicpc.net/problem/18231
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+k = int(input())
+destroys = list(map(int, input().split()))
+destroys_set = set(destroys)
+
+possibles = []
+for destroy in destroys:
+    if all(near in destroys_set for near in graph[destroy]):
+        possibles.append(destroy)
+
+real = set()
+for possible in possibles:
+    real.add(possible)
+    for near in graph[possible]:
+        real.add(near)
+
+if len(real) == len(destroys):
+    print(len(possibles))
+    print(' '.join(map(str, possibles)))
+else:
+    print(-1)
