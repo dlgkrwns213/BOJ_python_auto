1379b063d82c9947e4025f4d3011e760d3226f40
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-07-02 15:19:23 +0900

    [level 3] Title: 합승 택시 요금, Time: 21.08 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,38 +1,39 @@
 from heapq import heappop, heappush
-INF = int(1e9)
+
+
+def dijkstra(graph, n, start):
+    INF = int(1e9)
+    
+    distances = [INF] * (n+1)
+    distances[start] = 0
+    
+    hq = []
+    heappush(hq, (0, start))
+    
+    while hq:
+        dist, now = heappop(hq)
+        
+        if distances[now] < dist:
+            continue
+            
+        for nxt, nd in graph[now]:
+            nxt_dist = nd + dist
+            
+            if distances[nxt] > nxt_dist:
+                distances[nxt] = nxt_dist
+                heappush(hq, (nxt_dist, nxt))
+                
+    return distances
 
 
 def solution(n, s, a, b, fares):
-    def dijkstra(start):
-        distances = [INF] * (n+1)
-        distances[start] = 0
-        
-        hq = []
-        heappush(hq, (0, start))
-
-        while hq:
-            dist, now = heappop(hq)
-            if distances[now] < dist:
-                continue
-                
-            for nxt, nd in graph[now]:
-                nxt_dist = nd + dist
-                if distances[nxt] > nxt_dist:
-                    distances[nxt] = nxt_dist
-                    heappush(hq, (nxt_dist, nxt))
-                    
-        return distances
-        
-        
-    
     graph = [[] for _ in range(n+1)]
     for u, v, w in fares:
         graph[u].append((v, w))
         graph[v].append((u, w))
         
-    s_distances = dijkstra(s)
-    a_distances = dijkstra(a)
-    b_distances = dijkstra(b)
-
-    answer = min(map(lambda x: s_distances[x] + a_distances[x] + b_distances[x], range(1, n+1)))
-    return answer
+    distances_s = dijkstra(graph, n, s)
+    distances_a = dijkstra(graph, n, a)
+    distances_b = dijkstra(graph, n, b)
+    
+    return min(map(lambda idx: distances_s[idx] + distances_a[idx] + distances_b[idx], range(n+1)))
