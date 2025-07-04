36dd1c4bdc7729cce3016f8c3e574fc874aa6830
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-07-04 12:42:01 +0900

    [level 2] Title: 타겟 넘버, Time: 385.90 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
 
 ### 성능 요약
 
-메모리: 80.6 MB, 시간: 4.35 ms
+메모리: 9.25 MB, 시간: 385.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 02일 18:47:12
+2025년 07월 04일 12:42:00
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -8,8 +8,10 @@ def solution(numbers, target):
 def backtracking(numbers, target, idx, make):
     global count
     if idx == len(numbers):
-        count += target == make
+        if target == make:
+            count += 1
         return
     
-    backtracking(numbers, target, idx+1, make + numbers[idx])
-    backtracking(numbers, target, idx+1, make - numbers[idx])    
+    backtracking(numbers, target, idx+1, make+numbers[idx])
+    backtracking(numbers, target, idx+1, make-numbers[idx])
+    
