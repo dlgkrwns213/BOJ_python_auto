60f65ff0f618a5798ab5a96fc40d280a3fdd81f3
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-07-04 13:00:44 +0900

    [level 3] Title: 광고 삽입, Time: 793.94 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/README.md
+++ 프로그래머스/3/72414. 광고 삽입/README.md
@@ -1,10 +1,10 @@
 # [level 3] 광고 삽입 - 72414 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72414) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72414?language=python3) 
 
 ### 성능 요약
 
-메모리: 51.3 MB, 시간: 766.40 ms
+메모리: 53.7 MB, 시간: 793.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 04일 11:44:00
+2025년 07월 04일 13:00:42
 
 ### 문제 설명
 
--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
@@ -1,37 +1,46 @@
-def time_to_num(time):
-    h, m, s = map(int, time.split(":"))
-    return h * 3600 + m * 60 + s
+def time2num(time):
+    hh, mm, ss = map(int, time.split(":"))
+    return hh * 3600 + mm * 60 + ss
 
 
-def num_to_time(num):
-    num, s = divmod(num, 60)
-    h, m = divmod(num, 60)
-    return f'{h:02}:{m:02}:{s:02}'
+def num2time(num):
+    num, ss = divmod(num, 60)
+    hh, mm = divmod(num, 60)
+    
+    return f'{hh:02}:{mm:02}:{ss:02}'
 
 
 def solution(play_time, adv_time, logs):
-    play, adv = map(time_to_num, (play_time, adv_time))
-    MAX = 36000
-    counts = [0] * MAX
+    play, adv = map(time2num, (play_time, adv_time))
     
-    # imos
+    counts = [0] * (play+1)
     for log in logs:
-        start, last = map(time_to_num, log.split("-"))
+        start, end = map(time2num, log.split("-"))
         counts[start] += 1
-        counts[last] -= 1
+        counts[end] -= 1
         
-    for time in range(1, MAX):
-        counts[time] += counts[time-1]
+    for i in range(1, play+1):
+        counts[i] += counts[i-1]
         
-    # prefix sum
-    for time in range(1, MAX):
-        counts[time] += counts[time-1]
+    for i in range(1, play+1):
+        counts[i] += counts[i-1]
         
-    max_count, max_time = counts[adv], 0
-    for start in range(play - adv):
+    max_start, mx = 0, counts[adv]
+    for start in range(1, play+1-adv):
         end = start + adv - 1
-        current = counts[end] - counts[start - 1]
-        if current > max_count:
-            max_count = current
-            max_time = start
-    return num_to_time(max_time)
+        now = counts[end] - counts[start-1]
+        if mx < now:
+            mx = now
+            max_start = start
+    
+    # max_count, max_time = counts[adv], 0
+    # for start in range(1, play - adv + 1):
+    #     end = start + adv - 1
+    #     current = counts[end] - counts[start-1]
+    #     if current > max_count:
+    #         max_count = current
+    #         max_time = start
+    # return num_to_time(max_time)
+
+
+    return num2time(max_start)
