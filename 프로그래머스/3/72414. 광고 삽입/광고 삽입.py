a7ca3df3fa1dde0e19cf1b88f23d85a1c4e4de09
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-07-04 11:43:36 +0900

    [level 3] Title: 광고 삽입, Time: 766.40 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
@@ -1,42 +1,37 @@
-def solution(play_time, adv_time, logs):
-    def time2num(time: str) -> int:
-        h, m, s = map(int, time.split(':'))
-        return 3600 * h + 60 * m + s
-    
-    
-    def num2time(num: int) -> str:
-        h, num = divmod(num, 3600)
-        m, s = divmod(num, 60)
-        time = list(map(lambda x: str(x) if x >= 10 else f'0{x}', [h, m, s]))
-        return ':'.join(time)        
-        
-    
-    pt = time2num(play_time)
-    at = time2num(adv_time)
-    logs = [tuple(map(time2num, log.split('-'))) for log in logs]
-    
-    # 시작 시각 사람수, 끝 시각 사람수 저장
-    starts, lasts = dict(), dict()
-    for start, last in logs:
-        starts[start] = starts.get(start, 0) + 1
-        lasts[last] = lasts.get(last, 0) + 1
-    
-    # now에 현재 보는 사람 수 저장, 누적합
-    prefix_sum = [0] * (pt+2)
-    now = 0
-    for time in range(pt+1):
-        now += starts.get(time, 0) - lasts.get(time, 0)
-        prefix_sum[time+1] = prefix_sum[time] + now
-        
-    # 광고 시간동안 보는 사람의 수: score
-    mx_score, ans_time = 0, -1
-    for time in range(at, pt+2):
-        score = prefix_sum[time] - prefix_sum[time-at]
-        if mx_score < score:
-            mx_score = score
-            ans_time = time - at
-            
-    return num2time(ans_time)
+def time_to_num(time):
+    h, m, s = map(int, time.split(":"))
+    return h * 3600 + m * 60 + s
 
+
+def num_to_time(num):
+    num, s = divmod(num, 60)
+    h, m = divmod(num, 60)
+    return f'{h:02}:{m:02}:{s:02}'
+
+
+def solution(play_time, adv_time, logs):
+    play, adv = map(time_to_num, (play_time, adv_time))
+    MAX = 360001
+    counts = [0] * MAX
+    
+    # imos
+    for log in logs:
+        start, last = map(time_to_num, log.split("-"))
+        counts[start] += 1
+        counts[last] -= 1
         
+    for time in range(1, MAX):
+        counts[time] += counts[time-1]
         
+    # prefix sum
+    for time in range(1, MAX):
+        counts[time] += counts[time-1]
+        
+    max_count, max_time = counts[adv], 0
+    for start in range(play - adv + 1):
+        end = start + adv - 1
+        current = counts[end] - counts[start-1]
+        if current > max_count:
+            max_count = current
+            max_time = start
+    return num_to_time(max_time)
