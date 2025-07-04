966b324c3f1639ee979d7f50c1515454eaa82ee2
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-07-04 23:12:53 +0900

    [Bronze IV] Title: 鉛筆 (Pencils), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/15474. 鉛筆 （Pencils）/鉛筆 （Pencils）.py
+++ 백준/Bronze/15474. 鉛筆 （Pencils）/鉛筆 （Pencils）.py
@@ -1,8 +1,6 @@
-from math import ceil
-
 N, A, B, C, D = map(int, input().split())
 
-cost_X = ceil(N / A) * B
-cost_Y = ceil(N / C) * D
+cost_X = (N+A-1) // A * B
+cost_Y = (N+C-1) // C * D
 
 print(min(cost_X, cost_Y))
