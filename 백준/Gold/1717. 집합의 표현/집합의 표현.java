13b711f18e1217ce4d83cb4a695bb82d192e29da
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-07-25 23:14:02 +0900

    [Gold V] Title: 집합의 표현, Time: 432 ms, Memory: 47280 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] parent = IntStream.range(0, n+1)
+                .toArray();
+
+        StringBuilder ans = new StringBuilder();
+        while (m-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            String t = st.nextToken();
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (t.equals("0"))
+                union(parent, a, b);
+            else
+                ans.append(find(parent, a) == find(parent, b) ? "YES" : "NO").append('\n');
+
+        }
+        System.out.println(ans);
+    }
+
+    public static int find(int[] parent, int x) {
+        if (x == parent[x])
+            return x;
+        return find(parent, parent[x]);
+    }
+
+    public static void union(int[] parent, int x, int y) {
+        int px = find(parent, x);
+        int py = find(parent, y);
+
+        if (px == py)
+            return;
+
+        if (px > py)
+            parent[py] = px;
+        else
+            parent[px] = py;
+    }
+}
