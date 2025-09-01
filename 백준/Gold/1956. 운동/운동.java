63384363ccdd546d4979b1b9c3b4e8a3bc46c744
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-09-01 21:27:54 +0900

    [Gold IV] Title: 운동, Time: 928 ms, Memory: 110552 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -16,9 +16,9 @@ public class Main {
         int v = ve[0];
         int e = ve[1];
 
-        List<int[]>[] graph = new ArrayList[v+1];
-        for (int i = 1; i <= v; i++)
-            graph[i] = new ArrayList<>();
+        int[][] distances = new int[v+1][v+1];
+        for (int[] line: distances)
+            Arrays.fill(line, INF);
 
         while (e-- > 0) {
             int[] abc = Arrays.stream(br.readLine().split(" "))
@@ -29,47 +29,26 @@ public class Main {
             int b = abc[1];
             int c = abc[2];
 
-            graph[a].add(new int[]{b, c});
+            distances[a][b] = c;
         }
 
-        int ans = INF;
-        for (int start = 1; start <= v; start++) {
-            ans = Math.min(ans, dijkstra(graph, v, start));
-        }
-
-        System.out.println(ans != INF ? ans : -1);
-    }
-
-    private static int dijkstra(List<int[]>[] graph, int v, int start) {
-        int[] distances = new int[v+1];
-        Arrays.fill(distances, INF);
-        distances[start] = 0;
-
-        PriorityQueue<int[]> hq = new PriorityQueue<>(Comparator.comparingInt(o -> o[0]));
-        hq.add(new int[]{0, start});
-
-        while (!hq.isEmpty()) {
-            int[] first = hq.poll();
-            int dist = first[0];
-            int now = first[1];
-
-            if (dist > 0 && now == start)
-                return dist;
-            if (distances[now] < dist)
-                continue;
-
-            for (int[] nxtNode: graph[now]) {
-                int nxt = nxtNode[0];
-                int nxtDist = nxtNode[1] + dist;
-
-                if (distances[nxt] > nxtDist) {
-                    distances[nxt] = nxtDist;
-                    hq.add(new int[]{nxtDist, nxt});
-                } else if (nxt == start)
-                    hq.add(new int[]{nxtDist, nxt});
+        for (int k = 1; k <= v; k++) {
+            for (int i = 1; i <= v; i++) {
+                for (int j = 1; j <= v; j++) {
+                    if (distances[i][k] != INF && distances[k][j] != INF)
+                        distances[i][j] = Math.min(distances[i][j], distances[i][k] + distances[k][j]);
+                }
             }
         }
 
-        return INF;
+        int ans = INF;
+        for (int i = 1; i <= v; i++) {
+            for (int j = i+1; j <= v; j++) {
+                if (distances[i][j] != INF && distances[j][i] != INF)
+                    ans = Math.min(ans, distances[i][j] + distances[j][i]);
+            }
+        }
+
+        System.out.println(ans != INF ? ans : -1);
     }
 }
