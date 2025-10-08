94f0ae4f60371ff7655c493fa9986b03a97b4718
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-10-08 12:10:13 +0900

    [Gold III] Title: 선진이의 겨울 왕국, Time: 108 ms, Memory: 34968 KB -BaekjoonHub

---
+++ 백준/Gold/11567. 선진이의 겨울 왕국/선진이의 겨울 왕국.py
@@ -0,0 +1,35 @@
+# https://www.acmicpc.net/problem/11567
+import sys
+from collections import deque
+input = sys.stdin.readline
+go = ((-1, 0), (1, 0), (0, 1), (0, -1))
+
+
+def bfs() -> bool:
+    q = deque()
+    q.append((r1, c1))
+
+    while q:
+        x, y = q.popleft()
+
+        for a, b in go:
+            nx, ny = x+a, y+b
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if board[nx][ny]:
+                if (nx, ny) == (r2, c2):
+                    return True
+                else:
+                    continue
+
+            board[nx][ny] = 'X'
+            q.append((nx, ny))
+
+
+n, m = map(int, input().split())
+board = [[1 if x == 'X' else 0 for x in input().rstrip()] for _ in range(n)]
+r1, c1 = map(lambda x: int(x)-1, input().split())
+r2, c2 = map(lambda x: int(x)-1, input().split())
+
+print('YES' if bfs() else 'NO')
