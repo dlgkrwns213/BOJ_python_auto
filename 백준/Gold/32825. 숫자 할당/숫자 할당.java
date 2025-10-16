a017bbb7d16cf1534222f0484516a786442fd072
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-10-16 23:23:32 +0900

    [Gold IV] Title: 숫자 할당, Time: 184 ms, Memory: 14788 KB -BaekjoonHub

--- 백준/Gold/32825. 숫자 할당/숫자 할당.java
+++ 백준/Gold/32825. 숫자 할당/숫자 할당.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 
@@ -21,22 +20,22 @@ public class Main {
            return;
        }
 
-       backtracking(wantNumbers,  new ArrayList<>(), 0, 0);
+       backtracking(wantNumbers,  new int[10], 0, 0);
         System.out.println(answer);
     }
 
-    private static void backtracking(int[] wantNumbers, ArrayList<Integer> makeNumbers, int used, int count) {
+    private static void backtracking(int[] wantNumbers, int[] makeNumbers, int used, int count) {
         if (count == 10) {
-            int a = makeNumbers.get(0);
-            int b = makeNumbers.get(1);
-            int c = makeNumbers.get(2);
-            int d = makeNumbers.get(3);
-            int e = makeNumbers.get(4);
-            int f = makeNumbers.get(5);
-            int g = makeNumbers.get(6);
-            int h = makeNumbers.get(7);
-            int i = makeNumbers.get(8);
-            int j = makeNumbers.get(9);
+            int a = makeNumbers[0];
+            int b = makeNumbers[1];
+            int c = makeNumbers[2];
+            int d = makeNumbers[3];
+            int e = makeNumbers[4];
+            int f = makeNumbers[5];
+            int g = makeNumbers[6];
+            int h = makeNumbers[7];
+            int i = makeNumbers[8];
+            int j = makeNumbers[9];
 
             int k = wantNumbers[6] - (i+j);
             int k_ = wantNumbers[2] - (c+g);
@@ -50,31 +49,28 @@ public class Main {
         }
 
         if (count == 3) {
-            int d = wantNumbers[4] - (makeNumbers.get(0) + makeNumbers.get(1) + makeNumbers.get(2));
+            int d = wantNumbers[4] - (makeNumbers[0] + makeNumbers[1] + makeNumbers[2]);
             if (d > 0 && (used & (1 << d)) == 0) {
-                makeNumbers.add(d);
+                makeNumbers[count] = d;
                 backtracking(wantNumbers, makeNumbers, used | (1 << d), count+1);
-                makeNumbers.remove(makeNumbers.size()-1);
             }
             return;
         }
 
         if (count == 7) {
-            int d = wantNumbers[4] - (makeNumbers.get(0) + makeNumbers.get(1) + makeNumbers.get(2));
-            int h = wantNumbers[5] - (makeNumbers.get(4) + makeNumbers.get(5) + makeNumbers.get(6));
+            int d = wantNumbers[4] - (makeNumbers[0] + makeNumbers[1] + makeNumbers[2]);
+            int h = wantNumbers[5] - (makeNumbers[4] + makeNumbers[5] + makeNumbers[6]);
             if (h > 0 && (used & (1 << h)) == 0 && d+h == wantNumbers[3]) {
-                makeNumbers.add(h);
+                makeNumbers[count] = h;
                 backtracking(wantNumbers, makeNumbers, used | (1 << h), count+1);
-                makeNumbers.remove(makeNumbers.size()-1);
             }
             return;
         }
 
         for (int i = 1; i < 14; i++) {
             if ((used & (1 << i)) == 0) {
-                makeNumbers.add(i);
+                makeNumbers[count] = i;
                 backtracking(wantNumbers, makeNumbers, used | (1 << i), count+1);
-                makeNumbers.remove(makeNumbers.size()-1);
             }
         }
     }
