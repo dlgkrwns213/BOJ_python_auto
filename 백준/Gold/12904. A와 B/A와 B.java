eb3c757beac3f3493a7a7554036d9716b70d9fd5
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-10-23 16:16:47 +0900

    [Gold V] Title: A와 B, Time: 112 ms, Memory: 14588 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        String t = br.readLine();
+
+        StringBuilder sb = new StringBuilder(t);
+
+        while (sb.length() > s.length()) {
+            if (sb.charAt(sb.length() - 1) == 'A') {
+                sb.deleteCharAt(sb.length() - 1);
+            } else {
+                sb.deleteCharAt(sb.length() - 1);
+                sb.reverse();
+            }
+        }
+
+        if (sb.toString().equals(s)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
