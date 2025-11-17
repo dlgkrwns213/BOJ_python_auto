8799aeedb842c88abad1afe1289b904200a33f15
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-11-17 22:07:30 +0900

    [Bronze IV] Title: Atrium, Time: 100 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Bronze/20353. Atrium/Atrium.java
+++ 백준/Bronze/20353. Atrium/Atrium.java
@@ -1,9 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println(Math.sqrt(sc.nextLong()) * 4);
+        System.out.println(Math.sqrt(Long.parseLong(br.readLine())) * 4);
     }
 }
