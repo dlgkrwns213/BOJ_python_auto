7b39242a59524c975bda78db25d22516ce43107e
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-12-07 23:49:04 +0900

    [Bronze II] Title: 모르고리즘 회장 정하기, Time: 56 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/30684. 모르고리즘 회장 정하기/모르고리즘 회장 정하기.py
+++ 백준/Bronze/30684. 모르고리즘 회장 정하기/모르고리즘 회장 정하기.py
@@ -1,2 +1,4 @@
-n = int(input())
-print(sorted([x for x in (input() for _ in range(n)) if len(x) == 3])[0])
+import sys
+input = sys.stdin.readline
+
+print(sorted([x for x in (input() for _ in range(int(input()))) if len(x) == 4])[0][:-1])
