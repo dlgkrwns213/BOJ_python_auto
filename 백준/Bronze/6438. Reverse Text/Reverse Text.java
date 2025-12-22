401a227da884facdb732bae3322225a746a384f1
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2025-12-22 17:32:10 +0900

    [Bronze III] Title: Reverse Text, Time: 100 ms, Memory: 13992 KB -BaekjoonHub

---
+++ 백준/Bronze/6438. Reverse Text/Reverse Text.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0)
+            System.out.println(new StringBuilder(br.readLine()).reverse());
+    }
+}
