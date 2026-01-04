0fd834cd22ba38cc9bf2d365ba924e29d5fd3a52
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2026-01-04 15:33:58 +0900

    [Silver V] Title: 비밀 편지, Time: 88 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/5426. 비밀 편지/비밀 편지.py
+++ 백준/Silver/5426. 비밀 편지/비밀 편지.py
@@ -2,14 +2,12 @@ import sys
 from math import sqrt
 input = sys.stdin.readline
 
-ans = []
 for _ in range(int(input())):
     s = input().rstrip()
 
+    ans = []
     n = int(sqrt(len(s)))
     for j in range(n-1, -1, -1):
         for i in range(n):
             ans.append(s[i*n+j])
-    ans.append('\n')
-
-print(''.join(ans))
+    print(''.join(ans))
