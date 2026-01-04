0109c5a7fedafa3ef7c2ac54a7ed436d534e3b52
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2026-01-04 15:32:03 +0900

    [Silver V] Title: 비밀 편지, Time: 204 ms, Memory: 19576 KB -BaekjoonHub

---
+++ 백준/Silver/5426. 비밀 편지/비밀 편지.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String s = br.readLine();
+
+            int n = (int) Math.sqrt(s.length());
+            StringBuilder ans = new StringBuilder();
+
+            for (int j = n-1; j >= 0; j--) {
+                for (int i = 0; i < n; i++)
+                    ans.append(s.charAt(i * n + j));
+            }
+
+            System.out.println(ans);
+        }
+    }
+}
