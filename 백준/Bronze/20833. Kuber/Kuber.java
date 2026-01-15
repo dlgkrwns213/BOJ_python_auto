d72d82898f343342e635e1de623c37f83a817eae
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2026-01-15 22:04:57 +0900

    [Bronze IV] Title: Kuber, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/20833. Kuber/Kuber.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(IntStream.rangeClosed(1, n)
+                .map(i -> i * i * i)
+                .sum());
+    }
+}
