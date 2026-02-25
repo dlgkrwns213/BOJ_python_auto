8ae25c7d2413b25bd74a58366111649f3a7a83f0
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2026-02-25 21:46:23 +0900

    [Bronze IV] Title: 11 月 (November), Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/27332. 11 月 （November）/11 月 （November）.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        
+        System.out.println((a + 7 * b <= 30) ? 1 : 0);
+    }
+}
