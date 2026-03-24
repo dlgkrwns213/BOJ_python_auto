b3530839503b4e9ed4aaa942519a9d617b79d3d7
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2026-03-24 17:35:08 +0900

    [Gold IV] Title: GREAT + SWERC = PORTO, Time: 1160 ms, Memory: 111480 KB -BaekjoonHub

--- 백준/Gold/10529. GREAT ＋ SWERC ＝ PORTO/GREAT ＋ SWERC ＝ PORTO.py
+++ 백준/Gold/10529. GREAT ＋ SWERC ＝ PORTO/GREAT ＋ SWERC ＝ PORTO.py
@@ -2,20 +2,10 @@ from itertools import permutations
 
 
 def is_possible(pmt):
-    pmt_str = [str(x) for x in pmt]
-
     total = 0
-    for word in words[:-1]:
-        make = ''.join(pmt_str[indexes[x]] for x in word)
-        if make[0] == '0':
-            return False
-        total += int(make)
-
-    last = ''.join(pmt_str[indexes[x]] for x in words[-1])
-    if last[0] == '0':
-        return False
-
-    return total == int(last)
+    for c, w in weight.items():
+        total += pmt[indexes[c]] * w
+    return total == 0
 
 
 n = int(input())
@@ -27,7 +17,29 @@ for word in words:
 uses = list(uses)
 indexes = {v: i for i, v in enumerate(uses)}
 
+leading = set(word[0] for word in words)
+weight = {c: 0 for c in uses}
+
+for word in words[:-1]:
+    digit = 1
+    for c in reversed(word):
+        weight[c] += digit
+        digit *= 10
+
+digit = 1
+for c in reversed(words[-1]):
+    weight[c] -= digit
+    digit *= 10
+
 count = 0
 for pmt in permutations(list(range(10)), len(uses)):
+    valid = True
+    for c in leading:
+        if pmt[indexes[c]] == 0:
+            valid = False
+            break
+    if not valid:
+        continue
     count += is_possible(pmt)
+
 print(count)
