a4efdbf9598abb51dd709afeb2b51f7fde7b51ed
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2026-03-29 21:47:05 +0900

    [Bronze II] Title: 수 정렬하기, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        boolean[] exists = new boolean[2002];
+
+        while (n-- > 0)
+            exists[Integer.parseInt(br.readLine())+1000] = true;
+
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < 2002; i++) {
+            if (exists[i])
+                answer.append(i-1000).append('\n');
+        }
+
+        System.out.println(answer);
+    }
+}
