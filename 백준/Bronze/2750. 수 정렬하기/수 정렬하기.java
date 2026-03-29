c79d705b837955d733620d2a95c22bac01bfd389
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2026-03-29 22:39:05 +0900

    [Bronze II] Title: 수 정렬하기, Time: 124 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,23 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        boolean[] exists = new boolean[2002];
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++)
+            numbers[i] = Integer.parseInt(br.readLine());
 
-        while (n-- > 0)
-            exists[Integer.parseInt(br.readLine())+1000] = true;
+        // bubbleSort(numbers);
+        selectionSort(numbers);
+        // insertionSort(numbers);
 
         StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < 2002; i++) {
-            if (exists[i])
-                answer.append(i-1000).append('\n');
-        }
-
+        for (int number: numbers)
+            answer.append(number).append('\n');
         System.out.println(answer);
+    }
+
+    private static void bubbleSort(int[] numbers) {
+        int n = numbers.length;
+
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-1-i; j++) {
+                if (numbers[j] > numbers[j+1]) {
+                    int tmp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = tmp;
+                }
+            }
+        }
+    }
+
+    private static void selectionSort(int[] numbers) {
+        int n = numbers.length;
+
+        for (int i = 0; i < n-1; i++) {
+            int minIdx = i;
+
+            for (int j = i+1; j < n; j++) {
+                if (numbers[j] < numbers[minIdx])
+                    minIdx = j;
+            }
+
+            int tmp = numbers[i];
+            numbers[i] = numbers[minIdx];
+            numbers[minIdx] = tmp;
+        }
+    }
+
+    private static void insertionSort(int[] numbers) {
+        int n = numbers.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = numbers[i];
+            int j = i-1;
+
+            while (j >= 0 && numbers[j] > key) {
+                numbers[j+1] = numbers[j];
+                j--;
+            }
+        }
     }
 }
