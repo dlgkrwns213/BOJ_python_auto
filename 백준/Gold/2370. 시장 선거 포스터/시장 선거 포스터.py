73e6bb44d8a0e5f804c9594a0ac207e2de1a95ac
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2026-04-05 03:21:36 +0900

    [Gold III] Title: 시장 선거 포스터, Time: 80 ms, Memory: 36936 KB -BaekjoonHub

--- 백준/Gold/2370. 시장 선거 포스터/시장 선거 포스터.py
+++ 백준/Gold/2370. 시장 선거 포스터/시장 선거 포스터.py
@@ -1,18 +1,41 @@
 import sys
+sys.setrecursionlimit(int(1e5))
 input = sys.stdin.readline
 
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+
 n = int(input())
-posters = [list(map(int, input().split())) for _ in range(n)]
+posters = [tuple(map(int, input().split())) for _ in range(n)]
 
-locations = set()
+coords = set()
 for poster in posters:
-    locations |= set(poster)
-indexes = {v: i for i, v in enumerate(sorted(locations))}
+    coords |= set(poster)
 
-m = len(indexes)
-colors = [0] * m
-for i, (s, e) in enumerate(posters):
-    for x in range(indexes[s], indexes[e]+1):
-        colors[x] = i+1
+coords = sorted(coords)
+comp = {x: i for i, x in enumerate(coords)}
 
-print(len(set(colors)))
+parent = list(range(len(coords)+1))
+visible = 0
+
+for i in range(n-1, -1, -1):
+    l, r = posters[i]
+    l = comp[l]
+    r = comp[r]
+
+    x = find(l)
+    seen = False
+
+    while x <= r:
+        seen = True
+        parent[x] = x+1
+        x = find(x)
+
+    if seen:
+        visible += 1
+
+print(visible)
