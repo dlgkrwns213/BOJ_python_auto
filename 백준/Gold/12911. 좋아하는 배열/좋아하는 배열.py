4830a313fed37374590b9c81b0c91be24fddc73e
dlgkrwns213 <46648033+dlgkrwns213@users.noreply.github.com>
2026-04-04 19:38:40 +0900

    [Gold II] Title: 좋아하는 배열, Time: 3028 ms, Memory: 37044 KB -BaekjoonHub

--- 백준/Gold/12911. 좋아하는 배열/좋아하는 배열.py
+++ 백준/Gold/12911. 좋아하는 배열/좋아하는 배열.py
@@ -2,16 +2,17 @@ MOD = 10**9 + 7
 
 n, k = map(int, input().split())
 
-dp = [[0]*(k+1) for _ in range(n)]
-dp[0] = [0] + [1]*k
+a = [0] + [1] * k
 for i in range(1, n):
-    total = sum(dp[i-1])
-
+    total = sum(a)
+    b = [0] * (k+1)
     for j in range(1, k+1):
-        dp[i][j] = total
+        b[j] = total
 
         for x in range(2*j, k+1, j):
-            dp[i][j] -= dp[i-1][x]
-            dp[i][j] %= MOD
+            b[j] -= a[x]
+            b[j] %= MOD
 
-print(sum(dp[-1]) % MOD)
+    a = b
+
+print(sum(a) % MOD)
